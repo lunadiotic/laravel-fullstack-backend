0aeead2875693c85db1b44e5ac89292243831ad2
lionatic <ahmadhakim404@gmail.com>
2021-04-20 12:25:08 +0700

    setup project seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'email_verified_at' => null
+        ]);
     }
 }
